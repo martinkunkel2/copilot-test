84dfe86f2a6574959ea33d3cd5ca5f80678a36e1
Martin Kunkel <martin.kunkel@elektrobit.com>
2022-07-06 06:35:15 +0000

    minor changes

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
 #include <cstdlib>
 #include <iostream>
 
-int main(int /*argc*/, char */*argv*/[])
+
+
+int main(int argc, char *argv[])
 {
     std::cout << "Hello world from copilot-test" << std::endl;
+
+    
+        
+
     return EXIT_SUCCESS;
 }
